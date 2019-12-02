3b720f8f1e95d135fc86cf461e9498aa531bab09
Cagatay Gursoy <caggursoy93@icloud.com>
2019-12-02 17:25:32 +0100

    Update new files

--- animalNet/trainAnim.py
+++ animalNet/trainAnim.py
@@ -10,7 +10,7 @@ matplotlib.use("Agg")
 # import the necessary packages
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
-# from keras.optimizers import Adam
+from keras.optimizers import Adam
 from keras.optimizers import SGD
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -38,10 +38,10 @@ args = vars(ap.parse_args())
 
 # initialize the number of epochs to train for, initial learning rate,
 # and batch size
-EPOCHS = 10
+EPOCHS = 50
 INIT_LR = 1e-3  #1e-3
 BS = 32
-trgIm = [30,30,3]
+trgIm = [75,75,3]
 noClass = 10
 
 # initialize the data and labels
@@ -111,26 +111,26 @@ print("Training size: ",str(int(100*trainX.size/data.size)),"%")
 print("Test size: ",str(int(100*testX.size/data.size)),"%")
 print("Validation size: ",str(int(100*valX.size/data.size)),"%")
 
-# # construct the image generator for data augmentation
-# aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-# 	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-# 	horizontal_flip=True, fill_mode="nearest")
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")
 
 # initialize the model
 print("[INFO] compiling model...")
 model = modelArch.build(width=trgIm[0], height=trgIm[1], depth=trgIm[2], classes=noClass)
-# opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-opt = SGD(lr=0.0001)
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+# opt = SGD(lr=0.0001)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
-# H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-# 	validation_data=(valX, valY), steps_per_epoch=len(trainX) // BS,
-# 	epochs=EPOCHS, verbose=1, shuffle=True, max_queue_size=10)
-H = model.fit(x=trainX, y=trainY, validation_data=(valX, valY), steps_per_epoch=None,
-	epochs=EPOCHS, verbose=1, shuffle=False)
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+	validation_data=(valX, valY), steps_per_epoch=len(trainX) // BS,
+	epochs=EPOCHS, verbose=1, shuffle=True, max_queue_size=10)
+# H = model.fit(x=trainX, y=trainY, validation_data=(valX, valY), steps_per_epoch=None,
+# 	epochs=EPOCHS, verbose=1, shuffle=False)
 
 # save the model to disk
 print("[INFO] serializing network...")
