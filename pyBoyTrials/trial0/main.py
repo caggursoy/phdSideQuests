84d5541b352ce9ca9e94c97b8cad7744a63eaebd
caggursoy <caggursoy93@icloud.com>
2020-04-28 14:27:46 +0200

    Commit pyboy trials and start pyMarkdown

--- pyBoyTrials/trial0/main.py
+++ pyBoyTrials/trial0/main.py
@@ -1,2 +1,5 @@
-
 import functions
+
+pyboy = functions.loadCartridge('/home/cagatay/Documents/repos/phdSideQuests/pyBoyTrials/ROMs/supermario.gb', emSpd = 1)
+mario = functions.startMario(pyboy)
+functions.playLoop(pyboy, mario)
