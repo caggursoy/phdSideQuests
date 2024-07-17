14078fbb5b4a640b46b732a201296b4f1ab79b12
Cagatay Gursoy <caggursoy93@icloud.com>
2024-07-17 12:53:58 +0200

    Update certificates.py
    Trying to add the auto-installer for certifi package. Not final yet

--- stuff/certificates.py
+++ stuff/certificates.py
@@ -5,8 +5,36 @@ try:
     import certifi, subprocess
     import urllib.request
 except:
-    import subprocess, sys
-    subprocess.call([sys.executable, '-m', 'pip', '--trusted-host', 'pypi.python.org', 'install', 'certifi'])
+    try:
+        import subprocess, sys
+        subprocess.call([sys.executable, '-m', 'pip', '--trusted-host', 'pypi.python.org', 'install', 'certifi'])
+    except:
+        try:
+            import requests, tarfile
+            from pathlib import Path
+            # variables
+            url = 'https://files.pythonhosted.org/packages/c2/02/a95f2b11e207f68bc64d7aae9666fed2e2b3f307748d5123dffb72a1bbea/certifi-2024.7.4.tar.gz'
+            download_path = str(Path.cwd() / 'certifi.tar.gz')
+            extract_path = str(Path.cwd() / 'certifi')
+            # Download the file with SSL verification disabled
+            response = requests.get(url, verify=False)
+            response.raise_for_status()
+            
+            # Save the downloaded file to the specified download path
+            with open(download_path, 'wb') as file:
+                file.write(response.content)
+            # Extract the tar.gz file
+            with tarfile.open(download_path, 'r:gz') as tar:
+                tar.extractall(path=extract_path)
+            # Run the extracted file
+            command_path = Path.cwd() / 'certifi' / 'certifi-2024.7.4'
+            # subprocess.run([sys.executable, str(command_path)], capture_output=True)
+            subprocess.run([sys.executable, 'setup.py', 'install'], cwd=command_path, capture_output=True, text=True)
+        except requests.exceptions.RequestException as e:
+            print(f"Error downloading file: {e}")
+        except tarfile.TarError as e:
+            print(f"Error extracting file: {e}")
+    ##
     import urllib.request
     import certifi
 # get the main certificate location
@@ -34,3 +62,5 @@ for zi_cert in zi_cert_names:
 # close everything
 cert_main_file.close()
 cert_main_file_write.close()
+
+##
